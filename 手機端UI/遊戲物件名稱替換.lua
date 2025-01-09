3347a206e48b8ae664487e666fdc0e4ec848a8c1
Tseting-nil <183197443+Tseting-nil@users.noreply.github.com>
2025-01-09 20:44:44 +0800

    添加每日任務--名稱更改

--- 手機端UI/遊戲物件名稱替換.lua
+++ 手機端UI/遊戲物件名稱替換.lua
@@ -1,28 +1,48 @@
 local player = game:GetService("Players").LocalPlayer;
 local playerGui = player.PlayerGui;
-local missionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("通行证任务"):WaitForChild("背景"):WaitForChild("任务列表")
+local gamepassmissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("通行证任务"):WaitForChild("背景"):WaitForChild("任务列表")
 --任務資料夾初始化名稱
 local function gamepassmissionnamechange()
 	-- 檢查最終對象是否存在
-	if missionnamelist then
+	if gamepassmissionnamelist then
 	spawn(function()
         for i = 1, 12 do
-            local gamepassmissionlist = missionnamelist:FindFirstChild("任务项预制体")
+            local gamepassmissionlist = gamepassmissionnamelist:FindFirstChild("任务项预制体")
             if gamepassmissionlist then
             	gamepassmissionlist.Name = tostring(i)
             else
                 --更新數據
                 game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\232\142\183\229\143\150\230\149\176\230\141\174"):FireServer()
-                print("任務--名稱--已全部更改")
+                print("通行證任務--名稱--已全部更改")
             end
         end
 	end)
 	else
-		print("任務--名稱--已重複更改")
+		print("通行證任務--名稱--已重複更改")
     end
 end
-
 gamepassmissionnamechange()
+
+local everydaymissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("每日任务"):WaitForChild("背景"):WaitForChild("任务列表")
+local function everydatmissionnamechange()
+	if everydaymissionnamelist then
+	spawn(function()
+		for i = 1, 7 do
+			local everydaymissionlist = everydaymissionnamelist:FindFirstChild("任务项预制体")
+			if everydaymissionlist then
+				everydaymissionlist.Name = tostring(i)
+			else
+				print("每日任務--名稱--已全部更改")
+			end
+		end
+	end)
+	else
+		print("每日任務--名稱--已重複更改")
+	end
+end
+everydatmissionnamechange()
+
+
 
 -- 定時檢查當時間為 UTC+8 的 00:00(遊戲更新時間)
 local function checkTimeAndRun()
@@ -35,6 +55,7 @@ local function checkTimeAndRun()
             if utcPlus8Time.hour == 0 and utcPlus8Time.min == 0 then
                 print("UTC+8 時間為 00:00，開始執行更新數據...")
                 gamepassmissionnamechange()
+				everydatmissionnamechange()
                 wait(60) -- 等待 60 秒，避免重複執行
             end
 
