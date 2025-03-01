853bcfeb04690ac8999955d9759fb26e94cf7b79
Tseting-nil <183197443+Tseting-nil@users.noreply.github.com>
2025-03-01 23:59:06 +0800

    2025/03/01 添加通行證獎勵資料夾初始化 添加自動領取通行證相關代碼

--- 手機端UI/任務自動領取.lua
+++ 手機端UI/任務自動領取.lua
@@ -5,6 +5,9 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Tseting-nil/-Cultivat
 local mainmission = playerGui.GUI:WaitForChild("主界面"):WaitForChild("主城"):WaitForChild("主线任务"):WaitForChild("按钮"):WaitForChild("提示").Visible
 local missionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("通行证任务"):WaitForChild("背景"):WaitForChild("任务列表")
 local everydaymissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("每日任务"):WaitForChild("背景"):WaitForChild("任务列表")
+local gamepassgiftnnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("背景"):WaitForChild("右侧界面"):WaitForChild("月通行证"):WaitForChild("背景"):WaitForChild("奖励区"):WaitForChild("奖励列表")
+-- ========================================================================== --
+-- 主線任務
 function mainmissionchack()
     mainmission = playerGui.GUI:WaitForChild("主界面"):WaitForChild("主城"):WaitForChild("主线任务"):WaitForChild("按钮"):WaitForChild("提示").Visible 
     --print(mainmission)
@@ -13,7 +16,8 @@ function mainmissionchack()
         game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\228\184\187\231\186\191\228\187\187\229\138\161"):FindFirstChild("\233\162\134\229\143\150\229\165\150\229\138\177"):FireServer()
     end
 end
-
+-- ========================================================================== --
+-- 每日任務
 function everydaymission()
     if everydaymissionnamelist then
         for _, child in ipairs(everydaymissionnamelist:GetChildren()) do
@@ -46,8 +50,10 @@ function everydaymission()
         end
     end
 end
-
+-- ========================================================================== --
+-- 通行證任務
 function gamepassmission()
+    local udpdata = false
     if missionnamelist then
         for _, child in ipairs(missionnamelist:GetChildren()) do
             if child:IsA("Frame") and child.Visible == true then
@@ -72,7 +78,10 @@ function gamepassmission()
                             }
                             game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\229\174\140\230\136\144\228\187\187\229\138\161"):FireServer(unpack(args))
                             --更新數據
-                            game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\232\142\183\229\143\150\230\149\176\230\141\174"):FireServer()
+                            if not udpdata then
+                                game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\232\142\183\229\143\150\230\149\176\230\141\174"):FireServer()
+                                udpdata = true
+                            end
                         end
                     end
                 end
@@ -80,10 +89,60 @@ function gamepassmission()
         end
     end
 end
+-- ========================================================================== --
+-- 通行證獎勳
+local function gamepassgiftdraw(num, havepadgamepass)
+    local args = {
+        [1] = 1, -- 免費通行證
+        [2] = num -- 等級
+    }
+    game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\233\162\134\229\143\150"):FireServer(unpack(args))
+    if havepadgamepass then
+        local args = {
+            [1] = 2, -- 付費通行證
+            [2] = num -- 等級
+        }
+        game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\233\162\134\229\143\150"):FireServer(unpack(args))
+    end
+end
+
+local padgamepassnamecheck = false
+
+function gamepassgiftget()
+    local padgamepass = game:GetService("Players").LocalPlayer.PlayerGui.GUI["\228\186\140\231\186\167\231\149\140\233\157\162"]["\229\149\134\229\186\151"]["\232\131\140\230\153\175"]["\229\143\179\228\190\167\231\149\140\233\157\162"]["\230\156\136\233\128\154\232\161\140\232\175\129"]["\232\131\140\230\153\175"]["\229\165\150\229\138\177\229\140\186"]["\229\165\150\229\138\177\229\136\151\232\161\168"]:GetChildren()[4]["\233\187\132\233\135\145"]["\232\131\140\230\153\175"]["\228\184\138\233\148\129"].Visible
+    if gamepassgiftnnamelist then
+        if padgamepass and not padgamepassnamecheck then
+            print("無付費通行證")
+            padgamepassnamecheck = true
+        end
+    end
+
+    -- 使用寫死的for迴圈遍歷通行證獎勳（最多50）
+    for index = 1, 50 do
+        local namegamepassgif = gamepassgiftnnamelist:WaitForChild("gamepassgift" .. tostring(index))  -- 直接尋找對應的gamepassgift
+        if namegamepassgif then
+            -- 分別等待所需的子物件
+            local giftgetgcheck = namegamepassgif:WaitForChild("进度预制体"):WaitForChild("进度").Visible
+            local giftgetgcheck2 = namegamepassgif:WaitForChild("免费"):WaitForChild("背景"):WaitForChild("领取图标").Visible
+
+            if giftgetgcheck and padgamepass and not giftgetgcheck2 then
+                gamepassgiftdraw(index, true)
+            elseif giftgetgcheck and not padgamepass and not giftgetgcheck2 then
+                gamepassgiftdraw(index, false)
+            elseif not giftgetgcheck then
+                print("目前沒有通行證獎勳可領取")
+                break
+            end
+        end
+    end
+end
+
+
 --[[
 --控制開關    
     mainmissionchack()
     everydaymission()
     gamepassmission()
+    gamepassgiftget()
 ]]
 
--- 手機端UI/遊戲物件名稱替換.lua
+++ 手機端UI/遊戲物件名稱替換.lua
@@ -1,63 +1,90 @@
 local player = game:GetService("Players").LocalPlayer;
 local playerGui = player.PlayerGui;
-local gamepassmissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("通行证任务"):WaitForChild("背景"):WaitForChild("任务列表")
 -- ========================================================================== --
 -- 任務資料夾初始化名稱
+local gamepassmissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("通行证任务"):WaitForChild("背景"):WaitForChild("任务列表")
+local gamepassnamecheck = false
 local function gamepassmissionnamechange()
-    local namecheck = false
 	-- 檢查最終對象是否存在
 	if gamepassmissionnamelist then
-	spawn(function()
-        for i = 1, 12 do
-            local gamepassmissionlist = gamepassmissionnamelist:FindFirstChild("任务项预制体")
-            if gamepassmissionlist then
-            	gamepassmissionlist.Name = tostring(i)
-            else
-                game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\232\142\183\229\143\150\230\149\176\230\141\174"):FireServer()
-                if not namecheck then
-                    print("通行證任務--名稱--已全部更改")
-                    namecheck = true
+        spawn(function()
+            for i = 1, 12 do
+                local gamepassmissionlist = gamepassmissionnamelist:FindFirstChild("任务项预制体")
+                if gamepassmissionlist then
+                    gamepassmissionlist.Name = tostring(i)
+                else
+                    if not gamepassnamecheck then
+                        game:GetService("ReplicatedStorage"):FindFirstChild("\228\186\139\228\187\182"):FindFirstChild("\229\133\172\231\148\168"):FindFirstChild("\230\156\136\233\128\154\232\161\140\232\175\129"):FindFirstChild("\232\142\183\229\143\150\230\149\176\230\141\174"):FireServer()
+                        print("通行證任務--名稱--已全部更改")
+                        gamepassnamecheck = true
+                    end
                 end
             end
-        end
-        namecheck = false
-	end)
-	else
-		--print("通行證任務--名稱--已全部更改")
+        end)
     end
 end
 gamepassmissionnamechange()
 -- ========================================================================== --
--- 每日任务資料夾初始化名稱
+-- 通行證獎勵資料夾初始化名稱
+local player = game:GetService("Players").LocalPlayer;
+local playerGui = player.PlayerGui;
+local gamepassgiftnnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("商店"):WaitForChild("背景"):WaitForChild("右侧界面"):WaitForChild("月通行证"):WaitForChild("背景"):WaitForChild("奖励区"):WaitForChild("奖励列表")
+local gamepassgiftnamecheck = false
+local function gamepassgiftnnamechange()
+    if gamepassgiftnnamelist then
+        spawn(function()
+            local index = 1
+            while true do
+                local gamepassgiftnlist = gamepassgiftnnamelist:GetChildren()  -- 獲取所有子物件
+                local found = false
 
-local everydaymissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("每日任务"):WaitForChild("背景"):WaitForChild("任务列表")
-local function everydatmissionnamechange()
-    local namecheck = false
-	if everydaymissionnamelist then
-	spawn(function()
-		for i = 1, 7 do
-			local everydaymissionlist = everydaymissionnamelist:FindFirstChild("任务项预制体")
-			if everydaymissionlist then
-				everydaymissionlist.Name = tostring(i)
-			else
-                if not namecheck then
-                    print("每日任務--名稱--已全部更改")
-                    namecheck = true
+                for _, item in ipairs(gamepassgiftnlist) do
+                    if item.Name == "月通行证奖励预制体" then
+                        item.Name = "gamepassgift" .. tostring(index)  -- 設定名稱
+                        index = index + 1
+                        found = true
+                    end
                 end
-			end
-		end
-        namecheck = false
-	end)
+
+                if not found then
+                    if not gamepassgiftnamecheck then
+                        print("通行證獎勵--名稱--已全部更改")
+                        gamepassgiftnamecheck = true
+                    end
+                    break
+                end
+            end
+        end)
+    end
+end
+gamepassgiftnnamechange()
+-- ========================================================================== --
+-- 每日任務資料夾初始化名稱
+local everydaymissionnamelist = playerGui.GUI:WaitForChild("二级界面"):WaitForChild("每日任务"):WaitForChild("背景"):WaitForChild("任务列表")
+local everydaynamecheck = false
+local function everydatmissionnamechange()
+	if everydaymissionnamelist then
+        spawn(function()
+            for i = 1, 7 do
+                local everydaymissionlist = everydaymissionnamelist:FindFirstChild("任务项预制体")
+                if everydaymissionlist then
+                    everydaymissionlist.Name = tostring(i)
+                else
+                    if not everydaynamecheck then
+                        print("每日任務--名稱--已全部更改")
+                        everydaynamecheck = true
+                    end
+                end
+            end
+        end)
 	else
 		--print("每日任務--名稱--已全部更改")
 	end
 end
 everydatmissionnamechange()
 
-
 -- ========================================================================== --
--- 世界關卡資料夾初始化名稱&添加模塊
-
+-- 世界關卡資料夾初始化名稱
 local schedule = player:WaitForChild("值"):WaitForChild("主线进度")
 local worldname = schedule:FindFirstChild("世界")
 local worldlevelsname = schedule:FindFirstChild("关卡")
